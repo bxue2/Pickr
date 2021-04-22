5eb3129d02b8c576585c73fb5c7a113407b50ade
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-04-22 09:48:59 -0700

    Express setup

---
+++ backend/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const routes = require('./routes');
+
+const { environment } = require('./config');
+const isProduction = environment === 'production';
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(express.json());
+
+//Security Middleware
+//Only use CORS in development
+if(!isProduction){
+    app.use(cors());
+}
+//Sets some headers for security reasons
+app.use(helmet({
+    contentSecurityPolicy: false
+}));
+
+//Set _csrf token, create req.csrfToken method
+app.use(
+    csurf({
+        cookie: {
+            secure: isProduction,
+            sameSite: isProduction && "Lax",
+            httpOnly: true,
+        },
+    })
+);
+
+app.use(routes);
+
+module.exports = app;
