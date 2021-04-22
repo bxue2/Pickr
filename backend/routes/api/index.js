5795f5fd21dc4ca0ec55770e06fd72812b291614
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-04-22 15:17:36 -0700

    Fixed app.use ordering, added user auth

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,5 +1,21 @@
 const router = require('express').Router();
 
+
+// GET /api/set-token-cookie
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+
+router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+  const user = await User.findOne({
+      where: {
+        username: 'Demo-lition'
+      },
+    })
+  setTokenCookie(res, user);
+  return res.json({ user });
+}));
+
 router.post('/test', (req, res) => {
     res.json({ requestBody: req.body });
 });
