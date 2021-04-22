acc8cc16c27e0a81b216941a6487d1cf41d37bfe
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-04-22 15:21:44 -0700

    Finished testing user auth functions

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
 
 
-// GET /api/set-token-cookie
+
+
+
+router.post('/test', (req, res) => {
+    res.json({ requestBody: req.body });
+});
+
+/** Testing User Auth functions
+ * // GET /api/set-token-cookie
 const asyncHandler = require('express-async-handler');
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
@@ -16,8 +24,27 @@ router.get('/set-token-cookie', asyncHandler(async (req, res) => {
   return res.json({ user });
 }));
 
-router.post('/test', (req, res) => {
-    res.json({ requestBody: req.body });
-});
+// GET /api/restore-user
+const { restoreUser } = require('../../utils/auth.js');
+router.get(
+  '/restore-user',
+  restoreUser,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
+// GET /api/require-auth
+const { requireAuth } = require('../../utils/auth.js');
+router.get(
+  '/require-auth',
+  requireAuth,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+ *
+ */
+
 
 module.exports = router;
