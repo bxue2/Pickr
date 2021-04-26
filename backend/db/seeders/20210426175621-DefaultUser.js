1fce636d2202c22e7821db93d7e2ac5623bf21a1
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-04-26 11:13:53 -0700

    Added default value for created/updatedAt

--- backend/db/seeders/20210426175621-DefaultUser.js
+++ backend/db/seeders/20210426175621-DefaultUser.js
@@ -2,16 +2,12 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+    return queryInterface.bulkInsert('Users', [
+      {
+        email: 'pickr@user.io',
+        username: 'Pickr',
+        hashedPassword: bcrypt.hashSync('pickPass@1'),
+      }], {returning: true});
   },
 
   down: (queryInterface, Sequelize) => {
