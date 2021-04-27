cf65657386e972ff7eacd524b713225ae986d4a1
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-04-27 11:00:41 -0700

    Fixing up profile page logic

---
+++ frontend/src/components/ProfilePage/ProfileNavBar.js
@@ -0,0 +1,35 @@
+
+
+const ProfileNavBar = ({pageDisplay, setPageDisplay}) => {
+
+    return (
+        <div className='profile-nav-bar'>
+            <ul>
+                <li>
+                    <div
+                        className={pageDisplay === 'about' ? 'active' : ''}
+                        onClick={() => setPageDisplay("about")}>
+                        About
+                    </div>
+                </li>
+                <li>
+                    <div
+                        className={pageDisplay === 'pictures' ? 'active' : ''}
+                        onClick={() => setPageDisplay("pictures")}>
+                        Pictures
+                    </div>
+                </li>
+                <li>
+                    <div
+                        className={pageDisplay === 'album' ? 'active' : ''}
+                        onClick={() => setPageDisplay("album")}>
+                        Album
+                    </div>
+                </li>
+            </ul>
+        </div>
+
+    )
+}
+
+export default ProfileNavBar;
--- frontend/src/components/ProfilePage/index.js
+++ frontend/src/components/ProfilePage/index.js
@@ -1,19 +1,36 @@
 import {useState, useEffect} from 'react';
 import {useParams} from 'react-router-dom';
-import {useSelector} from 'react-redux';
+// import {useSelector} from 'react-redux';
 import { csrfFetch } from '../../store/csrf';
 import ProfileHeader from './ProfileHeader';
+import ProfileNavBar from './ProfileNavBar';
 
 const ProfilePage = () => {
-    let sessionUser = useSelector(state => state.session.user)
+    // let sessionUser = useSelector(state => state.session.user)
     const [user, setUser] = useState({});
+    const [pageDisplay, setPageDisplay] = useState("about");
     const params = useParams();
-    useEffect(async () => {
-        let profileUser = await csrfFetch(`/api/users/${params.userid}`)
-    }, [user])
+    useEffect(() => {
+        async function getUserData(){
+            let profileUser = await csrfFetch(`/api/users/${params.userid}`)
+            let data = await profileUser.json();
+            setUser(data);
+        }
+        getUserData();
+    }, [params.userid])
+
+    // const showComponent = () => {
+    //     switch(pageDisplay){
+    //         case "about":
+    //             return (<AboutComponent />)
+    //     }
+    // }
+
     return (
         <>
-            <ProfileHeader />
+            <ProfileHeader user={user}/>
+            <ProfileNavBar pageDisplay={pageDisplay} setPageDisplay={setPageDisplay}/>
+
         </>
     );
 }
