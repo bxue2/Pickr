72acf24ffe576233af8e4de4bce3f79db901797e
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-04-30 00:04:23 -0700

    Starting pictures reducer

--- frontend/src/store/pictures.js
+++ frontend/src/store/pictures.js
@@ -1,6 +1,58 @@
-export const LOAD_ITEMS = "pictures/LOAD_PICTURES";
-export const REMOVE_ITEM = "pictures/REMOVE_PICTURES";
-export const UPDATE_ITEM = "items/UPDATE_PICTURES";
-export const ADD_ITEM = "items/ADD_PICTURES";
+export const LOAD_ALBUM = "pictures/LOAD_ALBUM";
+export const REMOVE_PICTURE = "pictures/REMOVE_PICTURE";
+export const ADD_PICTURE = "items/ADD_PICTURE";
+export const CLEAR_PICTURES = "items/CLEAR_PICTURES"
 
 //Will store current list of photos
+
+const initialState = { pictures: []}
+
+const loadAlbum = (albumid) => {
+    return {
+        type: LOAD_PICTURES,
+        albumid
+    }
+}
+
+const removePicture = (pictureid) => {
+    return {
+        type: REMOVE_PICTURE,
+        pictureid
+    }
+}
+
+const addPicture = (pictureid) => {
+    return {
+        type: ADD_PICTURE,
+        pictureid
+    }
+}
+
+const clearPictures = () => {
+    return{
+        type: CLEAR_PICTURES
+    }
+}
+
+
+const picturesReducer = (state=initialState, action) => {
+    let newState = {...state};
+    switch(action.type){
+        case LOAD_ALBUM:
+            newState.user = action.user;
+            return newState;
+        case CLEAR_PICTURES:
+            newState = [];
+            return newState;
+        case REMOVE_PICTURE:
+            newState = [];
+            return newState;
+        case ADD_PICTURE:
+            newState.push("temp");
+            return newState;
+        default:
+            return state;
+    }
+}
+
+export default picturesReducer;
