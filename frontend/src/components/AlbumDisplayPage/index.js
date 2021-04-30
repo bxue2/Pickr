eba847ca6142e0fc7a38a069599f941aff35553c
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-04-30 10:44:46 -0700

    Fixing imports

--- frontend/src/components/AlbumDisplayPage/index.js
+++ frontend/src/components/AlbumDisplayPage/index.js
@@ -1,22 +1,25 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import {useParams} from 'react-router-dom';
+import {csrfFetch} from '../../store/csrf';
 
 const AlbumDisplayPage = () => {
     const [albumName, setAlbumName] = useState('');
     const [description, setDescription] = useState('');
+    const [albumOwnerId, setAlbumOwnerId] = useState(-1);
+    const [username, setUsername] = useState('');
     const {albumid} = useParams();
+
     useEffect(() => {
         async function fetchAlbum(){
-            let response = await csrfFetch(`/api/albums/${pictureid}`);
+            let response = await csrfFetch(`/api/albums/${albumid}`);
             let data = await response.json();
-            setImageUrl(data.image_url);
-            setName(data.name);
+            setAlbumName(data.name);
             setDescription(data.description);
-            setImageOwnerId(data.user_id);
+            setAlbumOwnerId(data.user_id);
             setUsername(data.user_name);
         }
         fetchAlbum();
-    }, [pictureid])
+    }, [albumid])
 
     return (
         <div className='album-display'>
