6882b05e15dd3ef555ca1ff9d24a8822ace2b4d0
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-04-30 15:27:55 -0700

    Adding tag form + hidden toggle

--- frontend/src/components/UploadImagePage/components/UploadBodySidebarTags.js
+++ frontend/src/components/UploadImagePage/components/UploadBodySidebarTags.js
@@ -1,9 +1,49 @@
+import {useState} from 'react';
+const UploadBodySidebarTags = ({tags, setTags}) => {
+    const [newTag, setNewTag] = useState('');
+    const [openTagForm, setOpenTagForm] = useState(false)
+    const UploadTagContainer = ({tagName}) => {
+        return (
+            <div className='upload-tag-container'>
+                {tagName}
+            </div>
+        )
+    }
 
-const UploadBodySidebarTags = () => {
+    const openAddTagForm = (e) => {
+        e.preventDefault();
+        setOpenTagForm(!openTagForm);
+    }
+
+    const addTag = (e) => {
+        e.preventDefault();
+        let newTagList = [...tags];
+        newTagList.push(newTag)
+        setTags(newTagList);
+        setNewTag('');
+    }
+
     return (
+        <>
         <div className='upload-sidebar-tags'>
-
+            <UploadTagContainer tagName='Bob'/>
+            <UploadTagContainer tagName='Bob2'/>
+            <div className='add-tag-button' onClick={openAddTagForm}>
+                <i className="far fa-plus-square"></i>
+            </div>
         </div>
+        <div className={`add-tag-form ${openTagForm ? '' : 'hidden'}`}>
+            <input
+            type='text'
+            value={newTag}
+            onChange={(e) => setNewTag(e.target.value)}
+            />
+            <div>
+                <button onClick={addTag}>Add Tag</button>
+                <button onClick={() => setOpenTagForm(false)}>Cancel</button>
+            </div>
+        </div>
+        </>
     )
 }
 
