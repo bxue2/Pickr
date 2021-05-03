29c423c39346a2896a7078b2f5465c16e797acad
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-05-02 17:13:53 -0700

    Added homepage images

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,13 +1,24 @@
 import {useSelector} from 'react-redux';
+import {useState, useEffect} from 'react';
 import {Redirect} from 'react-router-dom';
+
+import './HomePage.css';
 const HomePage = () => {
     let sessionUser = useSelector(state => state.session.user)
+    const [bgImageId, setBgImageId] = useState(0);
+    useEffect(() => {
+        setTimeout(() => {setBgImageId((bgImageId + 1)%3)}, 7000)
+    }, [bgImageId])
+
     if(sessionUser){
         return <Redirect to={`/users/${sessionUser.id}`}/>;
     }
     else{
         return (
-            <div className='home-display'>
+            <div
+                className='home-display'
+                style={{backgroundImage: ('url(' + require(`../../images/backgrounds/bg_${bgImageId}.jpg`).default + ')')}}
+            >
                 Welcome to Pickr
             </div>
         );
