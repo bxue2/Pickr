3fc08373acf8c1a2f00b3d0d1d91bb0c661f4383
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-05-03 08:39:02 -0700

    Added linkedin to about

--- frontend/src/components/AboutModal/AboutDiv.js
+++ frontend/src/components/AboutModal/AboutDiv.js
@@ -8,6 +8,10 @@ const AboutDiv = () => {
             <h4>Brian Xue</h4>
             <div className='profile-pic' />
             <span>
+            <span>
+                <h3>{"LinkedIn: "}</h3>
+                <Link to='https://www.linkedin.com/in/brian-xue/'>https://www.linkedin.com/in/brian-xue/</Link>
+            </span>
                 <h3>{"Github: "}</h3>
                 <Link to='https://github.com/bxue2'>https://github.com/bxue2</Link>
             </span>
